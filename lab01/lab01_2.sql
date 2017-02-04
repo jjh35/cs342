b47d35307511ddc9bcd1a064809281dc091fd7c3
jjh35 <jjh35@students.calvin.edu>
2017-02-03 19:32:57 -0500

    Create lab01_2.sql

---
+++ lab01/lab01_2.sql
@@ -0,0 +1,6 @@
+-- exercise 1.2 Jesse Hulse
+--Dual has one column, dummy that is of type VARCHAR2
+--it is a default table that can be used for initial practice with a sql. 
+--it can also be used as a reference when one is first starting.
+describe dual;
+select * from dual;
