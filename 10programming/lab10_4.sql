47a4bcae9288fef9c4f5563ac2a11e381954065a
unknown <jjh35@students.calvin.edu>
2017-04-19 20:59:24 -0400

    lab10_4 update

--- 10programming/lab10_4.sql
+++ 10programming/lab10_4.sql
@@ -1,10 +1,10 @@
 
---Concurrently running the original procedure in two different consoles did not work. I first set the rank equal to 1, and then ran the procedure twice concurrently
---At the end, the rank should have been 10,001, but instead it was  51,288. The issue is that the updates are interleaved, so when when procudre one graps the rank
---value (call it y) updates it (now its y+1) but then the second process takes control and updates it, say 5 times. Then the first process graps the rank value and it should
---be y+1, but now its y+6. So the value is incorrectly increased. I would say this is some type of lost update problem, but it does not fit the description in the
---book .
---I added the for update clause at the end of my select statement, and this fixed the issue
+--Concurrently running the original procedure in two different consoles did not work. I first set the rank of 176712 equal to 8.5, and then ran the procedure twice concurrently
+--with deltaIn set at 0.1. At the end, the rank should have been 10,008.5, but instead it was  6921.9. The issue is that the updates are interleaved, so the first procedure graps the rank
+--value (call it y), but then the second process reads the value (y) and both increment it by 0.1, then they both write the value, and the second one overwrites the first.
+--This is an example of the lost update problem.
+
+--I added the "for update" clause at the end of my select statement, and this fixed the issue
 CREATE OR REPLACE PROCEDURE incrementRank
 	(movieIdIn IN Movie.id%type, 
 	 deltaIn IN integer
@@ -13,10 +13,11 @@ CREATE OR REPLACE PROCEDURE incrementRank
 BEGIN
 	FOR i IN 1..50000 LOOP
 		SELECT rank INTO x FROM Movie WHERE id=movieIdIn for update;
+		--dbms_output.put_line(x);
 		UPDATE Movie SET rank=x+deltaIn WHERE id=movieIdIn;
 		COMMIT;
 	END LOOP;
 END;
 /
 
---EXECUTE incrementRank(238071, 1);
+--EXECUTE incrementRank(176712, 0.1);
