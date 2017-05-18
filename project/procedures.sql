8cabe1938ee3ccacb1c1c81f72db3257953e672f
unknown <jjh35@students.calvin.edu>
2017-05-17 22:09:04 -0400

    final project submission

--- project/procedures.sql
+++ project/procedures.sql
@@ -1,4 +1,6 @@
---This procedure will go the the player score table and calcualte the players' score based on their goals, assits, etc
+--This procedure will go the the player score table and calcualte the players' score based on their goals, assits, etc and update the player_score table
+--with the calculated score.
+--inWeek is the week number (game week) 
 create or replace procedure findPlayerScores(inWeek IN match_up.week_number%type) as
 score integer;
 goal_points integer;
@@ -6,6 +8,10 @@ played_points integer;
 assist_points integer;
 clean_sheet_points integer;
 begin
+	savepoint sp1;
+	--This procedure will be updating a significant chunk (one week's worth) of the player_score table without selecting anything from the player_score. 
+	--When we run this procedure, we do not want another thread to query the table because the values may not not be correct yet. So place a lock on the table.
+	Lock table player_score in row exclusive mode nowait;
 		for player in (select id from player) loop
 			select 5 * sum(goals) into goal_points from player_score where player_score.week = inWeek and player.id = player_score.player_id;
 			select 2 * played into played_points from player_score where player_score.week = inWeek and player.id = player_score.player_id;
@@ -13,23 +19,30 @@ begin
 			select 4 * nvl(clean_sheet,0) into clean_sheet_points from player_score where player_score.week = inWeek and player.id = player_score.player_id;
 			update player_score set score = goal_points + played_points + assist_points + clean_sheet_points where player_score.week = inWeek and player_score.player_id = player.id;
 		end loop;
+	commit;
+	EXCEPTION
+    WHEN OTHERS THEN
+		RAISE_APPLICATION_ERROR(-6501, 'error!');
+        rollback to savepoint sp1;
 end;
 /
 
 
---thie procedure will calculate the match_up scores for a given week;
+--thie procedure will calculate the match_up scores for a given week by adding up the players' scores on the given week and updating the match_up table
+--inWeek the week (game week)
 create or replace procedure findTeamScores(inWeek IN match_up.week_number%type) as
 	p_score integer;
 	team_score integer;
 begin
+	--This procedure updates the match_up scores without querying the match_up table, so we want to lock it to keep other threads from accessing it.
+	Lock table match_up in row exclusive mode nowait;
+	savepoint sp1;
 	team_score := 0;
 	for fantasy_team in (select distinct team1_ID from match_up where week_number = inWeek) loop
-
 			for player in (select distinct player_id from team_player where team_player.team_id = fantasy_team.team1_ID) loop
 				select score into p_score from player_score where player_score.player_id = player.player_id and player_score.week = inWeek;
 				team_score := team_score + p_score;
 			end loop;
-			
 		update match_up set team1_score = team_score where week_number = inWeek and team1_id = fantasy_team.team1_id;
 		team_score := 0;
 	end loop;
@@ -41,12 +54,17 @@ begin
 		update match_up set team2_score = team_score where week_number = inWeek and team2_id = fantasy_team.team2_id;
 		team_score := 0;
 	end loop;
+	commit;
+	EXCEPTION
+    WHEN OTHERS THEN
+		RAISE_APPLICATION_ERROR(-6501, 'error!');
+        rollback to savepoint sp1;
 end;
 /
 
-execute findPlayerScores(1);
-execute findTeamScores(1);
+--execute findPlayerScores(1);
+--execute findTeamScores(1);
 
 --test queries
-select player_Id as player, score as score, goals as goals, played as played, assists as asssits, clean_sheet as clean_sheet, week as week from player_score where week = 1;
-select distinct team1_score as team1_score, team1_ID as team1_id, team2_score as team2_score, team2_id as team2_id from match_up where week_number = 1;
+--select player_Id as player, score as score, goals as goals, played as played, assists as asssits, clean_sheet as clean_sheet, week as week from player_score where week = 1;
+--select distinct team1_score as team1_score, team1_ID as team1_id, team2_score as team2_score, team2_id as team2_id from match_up where week_number = 1;
--- project/queries.sql
+++ project/queries.sql
@@ -2,6 +2,8 @@
 
 --For a given user's team on a given week, select the players' name, team, and score.
 --A user who wants to check how his or her team is doing will want this query.
+--Alternate forms: This is query is just a multi-inner join, so other ways would be more complicated. I could have used an in(select...) but a simple
+--join is better
 select p.name || ' ' ||  p_t.name ||' ' || p_s.score
 	from player p, user_profile u_p, user_team u_t, player_score p_s, Team_Player t_p, pro_team p_t
 	where u_p.id = 1 
@@ -15,6 +17,8 @@ select p.name || ' ' ||  p_t.name ||' ' || p_s.score
 		
 --For a given week and given user's team, select the total score for that team
 --A user will want to check the total score for a team.
+--Alternate form: this multi-inner join is grouped by user profile name, week, and user_team name, so I can't think of an alternate way of doing it,
+--other than using the join ... on ... syntax or using a less optimal nested subquery
 select 'user ' || u_p.name ||  ' has ' || 'team ' || u_t.name || ' that scored ' ||  sum(p_s.score) || ' points for week ' || p_s.week
 	from player p, user_profile u_p, user_team u_t, player_score p_s, Team_Player t_p, pro_team p_t
 	where u_p.id = 1 
@@ -32,6 +36,7 @@ select 'user ' || u_p.name ||  ' has ' || 'team ' || u_t.name || ' that scored '
 --This view allows a person to check on the performance of unowned players by showing their scores per week.
 --If a person wants to make a transfer, then they will want to look at this view.
 --This should be a non-materalized view because it changes often, so it should be re-run each time the user wants to view it.
+--Alternate form: This query requires a not in, so I can't think of an alternate way of doing it
 create or replace view unowned_players as 
 	select distinct p.name as player_name, p_t.name as team_name, p_s.week as week, p_s.score as score
 	From player p, player_score p_s, pro_team p_t
@@ -47,12 +52,15 @@ create or replace view unowned_players as
 	 
 --This select finds the average scores for unowend players.
 --This will be useful for people wanting to make transfers.
+--Alternate method: This query is simple, and their isn't a better way of doing it.
 select p.player_name, AVG(p.score)
 	from unowned_players p
 	group by p.player_name;
 	
 --This query retrieves all match_up scores for a given week. (if a match_up has not started yet, then the scores will be null).
 --People who want to check the performance of their team will user this query.
+--Alternate form: this is a a multi-inner join with a not null check, I can't think of a better way of doing this. 
+--One could use a nested subquery, but this wouold not be optimal
 select  m_u.team2_ID as opponent, m_u.team1_score as your_score, m_u.team2_score as opponent_score
 	from match_up m_u, user_team u_t
 	where m_u.team1_score is not null 
@@ -61,7 +69,8 @@ select  m_u.team2_ID as opponent, m_u.team1_score as your_score, m_u.team2_score
 	
 	
 --This query returns a list of a user's teams and what league they are in. If any on the teams are not in a league, print out a null value for the league name.
---A user might want to check what league their various teams are in.	
+--A user might want to check what league their various teams are in.
+--Alternate forms: this nested inner and outer join statement is complicated, and I cannot think of a better/different way of this this.
 select u_t.name as team, l.name as league
 	from  User_profile u_p join 
 		(User_team ut join 
@@ -71,6 +80,7 @@ select u_t.name as team, l.name as league
 	
 --This query prints out the user and their reference, and it will print null if there is no reference.
 --The administator might want to check this, or a user may be curious about who they were references for.
+--Alternate method: This is a self-joining query. One could do a nested subquery, but again this is not optimal
 select u_p.name as user_profile, reference.name as reference
 from user_profile u_p left outer join user_profile reference on u_p.reference = reference.id;
 
