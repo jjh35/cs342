ec7bcef21a40d60b19417f253dc4c5375bdf07cb
unknown <jjh35@students.calvin.edu>
2017-05-17 22:42:11 -0400

    fixed documentation

--- project/nosql/readme.txt
+++ project/nosql/readme.txt
@@ -1,10 +1,4 @@
-import oracle.kv.*;
-
-import java.util.Arrays;
-import java.util.Map;
-
-public class Main {
-    /**
+/**
      * Answers to questions:
      * "explain why the information they provide is appropriate for a noSQL solution":
      *     Since players and player_score table could become very large in a real fantasy soccer league Database,
@@ -30,30 +24,6 @@ public class Main {
      *  	\player\ID\-\position
      *  	etc
 	 *
-	 *This main() calls on the LoadDB class (and methods) to load the sql DB into the kvlite DB
+	 *This Class implements methods that pull relations from a relational DB and translates them to a key-value kvlite database.
+	 *
      */
-
-
-    public static void main(String[] args)
-    {
-        try {
-            LoadDB load = new LoadDB();
-            //load table player_score
-            load.loadPlayerScores();
-            load.getPlayerScore("5", "2");
-            //load table player
-            load.loadPlayers();
-            load.getPlayer("10");
-            //Sorted list
-            load.getSortedPlayerScores("1");
-            //Useful two table Join
-            load.loadTeamPlayers();
-            load.getTeamNames("1");
-            load.getTeamNames("2");
-        }
-        catch(Exception e)
-        {
-            System.out.println("error: " + e);
-        }
-    }
-}
--- project/nosql/src/LoadDB.java
+++ project/nosql/src/LoadDB.java
@@ -4,8 +4,34 @@ import java.sql.*;
 import java.util.*;
 
 /**
- * Created by jjh35 on 5/12/2017.
- */
+     * Answers to questions:
+     * "explain why the information they provide is appropriate for a noSQL solution":
+     *     Since players and player_score table could become very large in a real fantasy soccer league Database,
+     *     a noSQL solution would be faster than a SQL solution.
+     *" evaluate whether or not Oracle's KVLite key-value system is the best type of noSQL database for your application"
+     *      KVLite would work well because the key-value structure works well (would be fast) for many of the queries that
+     *      this system would need to perform.
+     *
+	 *	Key structure for each table:
+	 *	The teamsNames table
+	 *   \teamNames\-teamName\teamName\player1Name
+     *   	\teamNames\-\teamID\teamName\player2Name
+     *       ...
+     *       \teamNames\-\teamID\teamName\player11Name
+	 *	Player score table
+	 *   \playerScore\-\week\playerID\score
+     *   	\playerScore\-\week\playerID\played
+     *  	\playerScore\-\week\playerID\goals
+     *   	etc
+	 *	Player table
+	 *	\player\ID\-\name
+     *  	\player\ID\-\proTeam
+     *  	\player\ID\-\position
+     *  	etc
+	 *
+	 *This Class implements methods that pull relations from a relational DB and translates them to a key-value kvlite database.
+	 *
+     */
 public class LoadDB {
 
     public LoadDB()
